3e5b7e40c36a8d54a3fe2fc71a3ed7c9677dcdf7
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-01-22 23:33:40 +0530

    Minor change to example.js for actually showing the output

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
 // Change './index' to 'is-null-or-empty' if you use this code outside of this package
 var { convertOnlineHostedFileToBase64, types } = require('./index');
 
-console.log(convertOnlineHostedFileToBase64("http://www.africau.edu/images/default/sample.pdf", types.pdf)); // content in base64
+convertOnlineHostedFileToBase64("http://www.africau.edu/images/default/sample.pdf", types.pdf).then(console.log); // content in base64
